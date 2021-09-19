64281521b0b496e2f0d7c49f1106343ded625bc5
ParaSpl01t <tushar10141@gmail.com>
2021-09-19 11:53:29 +0530

    Update archer.sh

--- archer.sh
+++ archer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Basic Variables
 VERSION=0.0.1
 BOOTMODE='bios'
 [[ -d /sys/firmware/efi ]] && BOOTMODE='uefi'
@@ -98,14 +100,21 @@ for i in ${!AURPKGSLIST[@]}; do
   AURPKGSLISTVAR+="$(($i+1)) ${AURPKGSLIST[$i]} "
 done
 
+# Install Dependencies
 pacman -S dialog
 
-#welcome dialog
+# Welcome dialog
 dialog --backtitle "archer.sh $VERSION" --title "ARCHER INSTALLATION" --msgbox "\nThis script will install a minimal GNOME setup with essential tools.\n\nBoot Mode : $BOOTMODE\n\nBefore Installation, make sure to partition and mount the disks and connect to Internet" 20 40
 
+clear
+
+# Enable parallel downloads
 sed -i '/Parallel/s/^#//g' /etc/pacman.conf
+
+# NTP
 timedatectl set-ntp true
 
+# Install base packages dialog
 BASEPKGS=$(dialog --backtitle "archer.sh $VERSION" --title "Base Packages" --checklist "\nChoose base packages to install:" 20 40 6 $BASEPKGSLISTVAR --output-fd 1)
 IFS=' ' read -r -a BASEPKGSARR <<< $BASEPKGS
 BASEPKGSTOINSTALL=''
@@ -114,29 +123,52 @@ do
 	IFS=' ' read -r -a basepgksfinal <<< ${BASEPKGSLIST[$(($element-1))]}
 	BASEPKGSTOINSTALL+="${basepgksfinal[0]} "
 done
+clear
+
+# Start base package installation
 pacstrap /mnt $BASEPKGSTOINSTALL git
 
+#genfstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# Enable paraller downloads in installation
 sed -i '/Parallel/s/^#//g' /mnt/etc/pacman.conf
 
+# Timezone dialog
 TIMEZONE=$(dialog --backtitle "archer.sh $VERSION" --title "Enter Timezone" --inputbox "\nEnter Timezone in Region/City format." 10 40 "Asia/Kolkata" --output-fd 1)
+clear
+
+# Set timezone
 $CHROOT timedatectl set-timezone $TIMEZONE
 
+# hwclock
 $CHROOT hwclock --systohc
+
+# Set language to en_US.utf-8
 cp /etc/locale* /mnt/etc/
+
+# Generate locale
 $CHROOT locale-gen
 
-
+# Hostname dialog
 HOSTNAME=$(dialog --backtitle "archer.sh $VERSION" --title "Enter Hostname" --inputbox "\nEnter the name of this machine." 10 40 "archer" --output-fd 1)
+clear
+
+# Set hostname
 echo $HOSTNAME > /mnt/etc/hostname
 echo "127.0.0.1\tlocalhost\n::1\t\t\tlocalhost\n127.0.1.1\t$HOSTNAME.localdomain\t$HOSTNAME" > /mnt/etc/hosts
 
+# Username dialog
 USERNAME=$(dialog --backtitle "archer.sh $VERSION" --title "Enter Username" --inputbox "\nEnter your username to login in this machine." 10 40 --output-fd 1)
+clear
 
+# Add user with wheel group
 $CHROOT useradd -mG wheel $USERNAME
 
+# Fetch paru-bin
 $CHROOT git clone https://aur.archlinux.org/paru-bin.git /home/$USERNAME/paru
+
+# Paru installation script
 echo "#!/bin/bash" > install.sh
 echo "cd /home/$USERNAME/paru" >> install.sh
 echo "chown -R $USERNAME:$USERNAME /home/$USERNAME/paru" >> install.sh
@@ -146,6 +178,7 @@ cp install.sh /mnt/home/$USERNAME/paru/
 $CHROOT chmod +x /home/$USERNAME/paru/install.sh
 $CHROOT /home/$USERNAME/paru/install.sh
 
+# Install GUI package dialog
 MAINPKGS=$(dialog --backtitle "archer.sh $VERSION" --title "GUI Packages" --checklist "\nChoose GUI packages to install:" 20 40 6 $MAINPKGSLISTVAR --output-fd 1)
 IFS=' ' read -r -a MAINPKGSARR <<< $MAINPKGS
 MAINPKGSTOINSTALL=''
@@ -154,9 +187,12 @@ do
 	IFS=' ' read -r -a mainpgksfinal <<< ${MAINPKGSLIST[$(($element-1))]}
 	MAINPKGSTOINSTALL+="${mainpgksfinal[0]} "
 done
+clear
+
+# Start GUI package installation
 $CHROOT pacman -S $MAINPKGSTOINSTALL
 
-
+# Install extra package dialog
 EXTRAPKGS=$(dialog --backtitle "archer.sh $VERSION" --title "Extra Packages" --checklist "\nChoose extra packages to install:" 20 40 6 $EXTRAPKGSLISTVAR --output-fd 1)
 IFS=' ' read -r -a EXTRAPKGSARR <<< $EXTRAPKGS
 EXTRAPKGSTOINSTALL=''
@@ -165,10 +201,15 @@ do
 	IFS=' ' read -r -a extrapgksfinal <<< ${EXTRAPKGSLIST[$(($element-1))]}
 	EXTRAPKGSTOINSTALL+="${extrapgksfinal[0]} "
 done
+clear
+
+# Start extra package installation
 $CHROOT pacman -S $EXTRAPKGSTOINSTALL
 
-$CHROOT rm -rf /home/$USERNAME/paru/*
+# Clenup paru
+$CHROOT rm -rf /home/$USERNAME/paru
 
+# Install AUR package dialog
 AURPKGS=$(dialog --backtitle "archer.sh $VERSION" --title "AUR Packages" --checklist "\nChoose AUR packages to install:" 20 40 6 $AURPKGSLISTVAR --output-fd 1)
 IFS=' ' read -r -a AURPKGSARR <<< $AURPKGS
 AURPKGSTOINSTALL=''
@@ -177,43 +218,49 @@ do
 	IFS=' ' read -r -a aurpgksfinal <<< ${AURPKGSLIST[$(($element-1))]}
 	AURPKGSTOINSTALL+="${aurpgksfinal[0]} "
 done
+clear
 
+# Create a script(run manually) to install selected AUR packages
 echo "#!/bin/bash" > aur.sh
 echo "kitty paru -S --skipreview $AURPKGSTOINSTALL" >> aur.sh
 echo "rm /home/$USERNAME/aur.sh" >> aur.sh
-echo "rm /home/$USERNAME/.config/autostart/aur.desktop" >> aur.sh
 cp aur.sh /mnt/home/$USERNAME/
 $CHROOT chmod +x /home/$USERNAME/aur.sh
 
-echo "[Desktop Entry]" > aur.desktop
-echo "Type=Application" >> aur.desktop
-echo "Name=aur-installer" >> aur.desktop
-echo "Exec=/home/$USERNAME/aur.sh" >> aur.desktop
-cp aur.desktop /mnt/home/$USERNAME/.config/autostart
-$CHROOT chmod +x /home/$USERNAME/.config/autostart/aur.desktop
+# AUR packages manual dialog
+dialog --backtitle "archer.sh $VERSION" --title "AUR Packages" --msgbox "\nPlease manually run ~/aur.sh to install AUR packages on system reboot." 10 40
+clear
 
-dialog --backtitle "archer.sh $VERSION" --title "AUR Packages" --msgbox "\nAUR packages will be installed on first system reboot." 10 40
-
+# Create root password
 $CHROOT echo "Create root password."
 $CHROOT passwd
-$CHROOT clear
+clear
+
+# Create user password
 $CHROOT echo "Create password for $USERNAME."
 $CHROOT passwd $USERNAME
+clear
 
+# /etc/sudoers file edit dialog
 dialog --backtitle "archer.sh $VERSION" --title "Edit /etc/sudoers" --yesno "\nPlease manually uncomment the '%wheel ALL=(ALL) ALL' line.\nDo you want to open the /etc/sudoers file?" 10 40
 response=$?
 clear
 case $response in
-   0) EDITOR=nano visudo;;
+   0) $CHROOT EDITOR=nano visudo;;
 esac
 
+# GRUB disk location
 GRUBDISK=$(dialog --backtitle "archer.sh $VERSION" --title "Install GRUB" --inputbox "\nEnter the disk to install grub on." 10 40 "/dev/sda" --output-fd 1)
+clear
 
+# Start GRUB installation
 $CHROOT grub-install --target=i386-pc $GRUBDISK
 $CHROOT grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable bluetooth
-systemctl enable gdm
-systemctl enable NetworkManager
+# Enable services
+$CHROOT systemctl enable bluetooth
+$CHROOT systemctl enable gdm
+$CHROOT systemctl enable NetworkManager
 
+# final dialog
 dialog --backtitle "archer.sh $VERSION" --title "INSTALLATION FINISHED" --msgbox "\nYou can now reboot the machine." 10 40
